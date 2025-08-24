38d3b8c3213d5329d46b1dc186ed95e6dacc31fe
KOkoj <matej.kucera03@seznam.cz>
2025-08-24 17:13:50 +0200

    Enhance blog post creation route with authorization and validation middleware

--- backend/src/routes/blog.routes.ts
+++ backend/src/routes/blog.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { authenticate } from '../middlewares/auth.js';
+import { authorize } from '../middlewares/authorize.js';
+import { validateBody } from '../middlewares/validate.js';
+import { createBlogPostSchema } from '../validation/blog.js';
 import { createPost, deletePost, getPost, listPosts, updatePost } from '../controllers/blog.controller.js';
 
 export const blogRouter = Router();
@@ -8,6 +11,6 @@ blogRouter.get('/', listPosts);
 blogRouter.get('/:id', getPost);
 
 blogRouter.use(authenticate);
-blogRouter.post('/', createPost);
+blogRouter.post('/', authorize('ADMIN', 'MANAGER'), validateBody(createBlogPostSchema), createPost);
 blogRouter.put('/:id', updatePost);
 blogRouter.delete('/:id', deletePost);
